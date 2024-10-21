1fa5b57e3f72e08fe700140b22bdca04f045544d
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-21 17:09:52 +0200

    update slurm scripts

--- numba_check.sh
+++ numba_check.sh
@@ -10,18 +10,12 @@
 #SBATCH -e %x-%j.err
 #SBATCH -o %x-%j.out
 
-
-module purge 
-module load 2023
-module load Python/3.11.3-GCCcore-12.3.0
-module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
-module load matplotlib/3.7.2-gfbf-2023a
-module load h5py/3.9.0-foss-2023a
-module load numba/0.58.1-foss-2023a
+cd /home/fhafner/repositories/layered_walk/
+source 2023_snel_modules.sh
 
 source .venv/bin/activate 
 
-python numba_check.py --location snellius
+python numba_check.py --location snellius 
 
 
 

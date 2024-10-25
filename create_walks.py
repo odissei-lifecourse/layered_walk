10beb85db525c3e547112ec315eae3a262af0049
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-25 15:58:37 +0200

    use refactored function from module

--- create_walks.py
+++ create_walks.py
@@ -1,24 +1,17 @@
 import asyncio
 import argparse
-import numpy as np
 import logging
 
 from src.utils import (
-    batched,
     load_data,
     convert_to_numba,
     get_n_cores,
     check_layer_edge_dict,
-    save_to_file,
 )
 from src.walks_numba import create_walks as create_walks_numba
-from src.walks import create_walks_starting_from_layers
+from src.walk_processor import process_nodes
 from config import data_dir
 from tqdm import tqdm
-import pyarrow as pa
-import pyarrow.parquet as pq
-from collections import defaultdict
-
 
 logger = logging.getLogger(__name__)
 
@@ -111,86 +104,22 @@ async def main():
 
     _ = walks_wrapper(users[:10])
 
-    ### --START this should go into its own file
-    async def walks_generator(users, n_workers):
-        coroutines = [
-            asyncio.to_thread(walks_wrapper, batch)
-            for batch in batched(users, len(users) // n_workers)
-        ]
-
-        for future in asyncio.as_completed(coroutines):
-            result_list = await future
-            for result in result_list:
-                yield {"SOURCE": result[0], **{f"STEP_{i}": step for i, step in enumerate(result[1:])}}
-
-        # result = await asyncio.gather(*(asyncio.to_thread(walks_wrapper, batch) for batch in batched(users, len(users)//n_workers)))
-        # return result
-
-    async def process_nodes(users, n_workers, filename):
-        # define schema
-        field_col0 = [pa.field("SOURCE", pa.int64())] 
-        other_fields = [pa.field(f"STEP_{i}", pa.int64()) for i in range(2*WALK_LEN)]
-        fields = field_col0 + other_fields
-        schema = pa.schema(fields)
-
-        results = [] 
-        chunk_count = 0
-        writer = None
-        async for result in walks_generator(users, n_workers):
-            results.append(result)
-
-            chunk_count += 1
-            if chunk_count >= 100_000:
-                writer = await write_chunk(results, writer, filename, schema)
-                results.clear()
-                chunk_count = 0
-
-        if results:
-            writer = await write_chunk(results, writer, filename, schema)
-
-        # add walks starting at node ids
-        additional_walks = create_walks_starting_from_layers(
-                layer_id_set = layer_id_set,
-                users=users,
-                walk_len=WALK_LEN,
-                n_walks=1,
-                layer_edge_dict=layer_edge_dict,
-                p=JUMP_PROB)
-
-        add_walks_writeable = []
-        for result in additional_walks:
-            temp_dict = {"SOURCE": result[0], **{f"STEP_{i}": step for i, step in enumerate(result[1:])}}
-            add_walks_writeable.append(temp_dict)
-
-        writer = await write_chunk(add_walks_writeable, writer, filename, schema)
-
-        if writer:
-            writer.close()
-
-
-    async def write_chunk(results, writer, filename, schema):
-        table = pa.Table.from_pylist(results)
-        if writer is None:
-            writer = pq.ParquetWriter(filename + ".parquet", schema)
-
-        writer.write_table(table)
-        return writer
-
-    ### --END-- this should go into its own file
-
     logger.info("Creating walks")
+
     for i in tqdm(range(N_WALKS)):
         filename = DATA_DIR["output"] + DEST + "_" + str(YEAR) + "_" + str(i)
-        
         if DRY_RUN:
             filename += "_dry"
         
-        #required_length = len(result[0][0])
-        #additional_walks = [x[:required_length] for x in additional_walks]
-        await process_nodes(users_numba, N_WORKERS, filename)
-
-        #logger.info("Saving")
-
+        await process_nodes(
+                walk_fct=walks_wrapper, 
+                users=users_numba,
+                walk_len=WALK_LEN,
+                layer_id_set=layer_id_set,
+                layer_edge_dict=layer_edge_dict_numba,
+                n_workers=N_WORKERS,
+                filename=filename,
+                jump_prob=JUMP_PROB)
 
     logger.info("Done.")
 

56d7b52f5e5a148bda0958a442b3c72064c471fa
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-25 14:20:29 +0200

    first trial to write async results as they arrive

--- create_walks.py
+++ create_walks.py
@@ -1,8 +1,7 @@
-
 import asyncio
 import argparse
 import numpy as np
-import logging 
+import logging
 
 from src.utils import (
     batched,
@@ -10,12 +9,16 @@ from src.utils import (
     convert_to_numba,
     get_n_cores,
     check_layer_edge_dict,
-    save_to_file
-) 
+    save_to_file,
+)
 from src.walks_numba import create_walks as create_walks_numba
-from src.walks import  create_walks_starting_from_layers
+from src.walks import create_walks_starting_from_layers
 from config import data_dir
 from tqdm import tqdm
+import pyarrow as pa
+import pyarrow.parquet as pq
+from collections import defaultdict
+
 
 logger = logging.getLogger(__name__)
 
@@ -29,24 +32,38 @@ LAYERS_DRY_RUN = ["household", "classmate"]
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--dry-run", 
-        dest="dry_run", 
+        "--dry-run",
+        dest="dry_run",
         help="If given, runs a test with a small output data size.",
-        action=argparse.BooleanOptionalAction
-        )  
-    parser.add_argument("--location", help="Snellius or local machine", choices=LOCATION_CHOICES)
-    parser.add_argument("--dest", help="Destination of csv file, relative to data_dir. year will be appended to the end.", type=str)
-    parser.add_argument("--n_walks", help="Number of walks per node", type=int, default=5)
-    parser.add_argument("--walk_len", help="Length of walks to generate", type=int, default=50)
-    parser.add_argument("--year", help="Which year of the network data to use", type=int, default=2010)
-    parser.add_argument("--debug", help="Debugging. Do additional checks.", 
-            default=False, action=argparse.BooleanOptionalAction)
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument(
+        "--location", help="Snellius or local machine", choices=LOCATION_CHOICES
+    )
+    parser.add_argument(
+        "--dest",
+        help="Destination of csv file, relative to data_dir. year will be appended to the end.",
+        type=str,
+    )
+    parser.add_argument(
+        "--n_walks", help="Number of walks per node", type=int, default=5
+    )
+    parser.add_argument(
+        "--walk_len", help="Length of walks to generate", type=int, default=50
+    )
+    parser.add_argument(
+        "--year", help="Which year of the network data to use", type=int, default=2010
+    )
+    parser.add_argument(
+        "--debug",
+        help="Debugging. Do additional checks.",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+    )
     return parser.parse_args()
 
 
-
 async def main():
-
     args = parse_args()
     DRY_RUN = args.dry_run
     LOCATION = args.location
@@ -60,10 +77,10 @@ async def main():
 
     logging_level = logging.DEBUG if DEBUG else logging.INFO
     logging.basicConfig(
-            format="%(asctime)s %(name)s %(levelname)s: %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-            level=logging_level
-            )
+        format="%(asctime)s %(name)s %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging_level,
+    )
 
     layers_to_load = LAYERS
     if DRY_RUN:
@@ -72,18 +89,18 @@ async def main():
     if DRY_RUN:
         sample_size = SAMPLE_SIZE_DRY_RUN
 
-    logger.info("Loading data")    
+    logger.info("Loading data")
     connected_node_file = "connected_user_set" if LOCATION == "ossc" else None
     users, layer_edge_dict, layer_id_set = load_data(
-        DATA_DIR["input"], YEAR, connected_node_file, layers_to_load, sample_size 
+        DATA_DIR["input"], YEAR, connected_node_file, layers_to_load, sample_size
     )
-    
+
     if DEBUG:
         check_layer_edge_dict(layer_edge_dict)
 
     logger.info("Converting to numba")
     users_numba, layer_edge_dict_numba = convert_to_numba(users, layer_edge_dict)
-    
+
     if DEBUG:
         check_layer_edge_dict(layer_edge_dict_numba)
 
@@ -94,39 +111,84 @@ async def main():
 
     _ = walks_wrapper(users[:10])
 
-    async def create_walks_parallel(users, n_workers):
-        result = await asyncio.gather(*(asyncio.to_thread(walks_wrapper, batch) for batch in batched(users, len(users)//n_workers)))
-        return result 
-    
+    ### --START this should go into its own file
+    async def walks_generator(users, n_workers):
+        coroutines = [
+            asyncio.to_thread(walks_wrapper, batch)
+            for batch in batched(users, len(users) // n_workers)
+        ]
+
+        for future in asyncio.as_completed(coroutines):
+            result_list = await future
+            for result in result_list:
+                yield {"SOURCE": result[0], **{f"STEP_{i}": step for i, step in enumerate(result[1:])}}
+
+        # result = await asyncio.gather(*(asyncio.to_thread(walks_wrapper, batch) for batch in batched(users, len(users)//n_workers)))
+        # return result
+
+    async def process_nodes(users, n_workers, filename):
+        # define schema
+        field_col0 = [pa.field("SOURCE", pa.int64())] 
+        other_fields = [pa.field(f"STEP_{i}", pa.int64()) for i in range(WALK_LEN)]
+        fields = field_col0 + other_fields
+        schema = pa.schema(fields)
+
+        results = [] 
+        chunk_count = 0
+        writer = None
+        async for result in walks_generator(users, n_workers):
+            results.append(result)
+
+            chunk_count += 1
+            if chunk_count >= 1000:
+                await write_chunk(results, writer, filename, schema)
+                results.clear()
+                chunk_count = 0
+
+        if results:
+            await write_chunk(results, writer, filename, schema)
+
+        # add walks starting at node ids
+        additional_walks = create_walks_starting_from_layers(
+                layer_id_set = layer_id_set,
+                users=users,
+                walk_len=WALK_LEN,
+                n_walks=1,
+                layer_edge_dict=layer_edge_dict,
+                p=JUMP_PROB)
+
+        await write_chunk(additional_walks, writer, filename, schema)
+
+        if writer:
+            writer.close()
+
+
+    async def write_chunk(results, writer, filename, schema):
+        table = pa.Table.from_pylist(results)
+        if writer is None:
+            writer = pq.ParquetWriter(filename + ".parquet", schema)
+
+        writer.write_table(table)
+        return writer
+
+    ### --END-- this should go into its own file
 
     logger.info("Creating walks")
     for i in tqdm(range(N_WALKS)):
-        result = await create_walks_parallel(users_numba, N_WORKERS)
-        additional_walks = create_walks_starting_from_layers(
-                layer_id_set=layer_id_set,
-                users=users,
-                walk_len=WALK_LEN,
-                n_walks=N_WALKS,
-                layer_edge_dict=layer_edge_dict,
-                p=JUMP_PROB
-                ) 
-        required_length = len(result[0][0])
-        additional_walks = [x[:required_length] for x in additional_walks]
-    
-        result.append(additional_walks)
-    
-        logger.info("Saving")
-        filename = DATA_DIR["output"] + DEST + "_" + str(YEAR) + "_" + i
+        filename = DATA_DIR["output"] + DEST + "_" + str(YEAR) + "_" + str(i)
+        
         if DRY_RUN:
             filename += "_dry"
-    
-        save_to_file(result, filename, "parquet")
+        
+        #required_length = len(result[0][0])
+        #additional_walks = [x[:required_length] for x in additional_walks]
+        await process_nodes(users_numba, N_WORKERS, filename)
+
+        #logger.info("Saving")
+
 
     logger.info("Done.")
 
 
-
 if __name__ == "__main__":
     asyncio.run(main())
-
-

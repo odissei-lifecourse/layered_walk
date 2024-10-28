262eef07ded7fa40bd5dfc7cc0b6a0d6324c27f0
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-28 09:56:16 +0100

    prototype for additional walks in numba

--- src/walks_numba.py
+++ src/walks_numba.py
@@ -60,6 +60,7 @@ def create_walks(
 def single_walk(start_node: types.int64,
                 walk_len: int, 
                 layer_edge_dict: numba.typed.Dict,
+                start_layer: int | None=None,
                 p: float=0.8):
     """Create a single random walk starting at one node.
     
@@ -73,18 +74,23 @@ def single_walk(start_node: types.int64,
     Returns:
         list: a sequence of node identifiers
     """
+   
     current_node = start_node
-
     walk = List.empty_list(types.int64)
     walk.append(start_node)
 
-    
-    layer_indices = np.array(list(layer_edge_dict[current_node].keys()))
-    layer_index = custom_sample(layer_indices)
-    if layer_index == -1:
-        msg = f"invalid layer index for node {current_node} with layer_indices {layer_indices}"
-        raise RuntimeError(msg)
 
+    if start_layer is None:
+        layer_indices = np.array(list(layer_edge_dict[current_node].keys()))
+        layer_index = custom_sample(layer_indices)
+    else:
+        layer_index = start_layer
+
+    if layer_index == -1:
+        msg = f"Invalid layer index for node {current_node} with layer indices {layer_indices}" 
+        raise ValueError(msg)
+    
+  
     for draw in np.random.rand(walk_len):
         layer_indices = np.array(list(layer_edge_dict[current_node].keys()))
 
@@ -106,3 +112,61 @@ def single_walk(start_node: types.int64,
         current_node = next_node
 
     return walk 
+
+
+@numba.njit(nogil=True)
+def create_walks_starting_from_layers(
+        layer_id_set: np.ndarray,
+        nodes: numba.int64[:],
+        walk_len: int, 
+        n_walks: int,
+        layer_edge_dict: numba.typed.Dict,
+        p: float=0.8):
+    """"Create one walk for each unique layer identifier.
+
+    Args:
+        layer_id_set (np.ndarray): Array of unique layer identifiers. One walk starting from each of them
+        will be created.
+        nodes (list): List of unique node identifiers.
+        walk_len (int): length of the walk to generate.
+        n_walks (int): Number of walks to generate for each layer.
+        layer_edge_dict: Dictionary where keys are node identifiers and values are dictionaries 
+        of non-empty edge lists for each layer.
+        p: probability of changing layer.
+
+    Returns:
+        list: a list of walks, one starting from each of the layer identifiers.
+    """
+
+    walks = []
+    for current_layer in np.tile(list(layer_id_set), n_walks):
+        np.random.shuffle(nodes)
+        start_node = None
+        
+        while start_node is None:
+            for current_node in nodes:
+                if current_layer in layer_edge_dict[current_node]:
+                    start_node = current_node 
+         
+        if start_node is None:
+            msg = f"Checked all nodes, and none of them had a connection in layer {current_layer}"
+            raise RuntimeError(msg)
+        
+        # invoke the walk function here with walk_len; crop at the end
+        regular_walk = single_walk(
+                start_node=start_node, 
+                walk_len=walk_len,
+                layer_edge_dict=layer_edge_dict,
+                start_layer=current_layer,
+                p=p)
+        walk = [current_layer] + regular_walk
+        expected_length = 1 + 2*walk_len
+        walk = walk[:expected_length]
+        walks.append(walk)
+    
+    return walks
+
+
+
+
+
